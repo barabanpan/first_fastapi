0209b68873af8f80df91d26a034da08212de4f6e
Nataliia Dyshko <nataliia.dyshko@ukr.net>
2021-05-29 00:06:03 +0300

    Add post and get for Employee model

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from datetime import date
+
+
+class EmployeeIn(BaseModel):
+    first_name: str
+    last_name: str
+    position: str
+    salary: float
+    married: bool
+    start_work: date
+
+
+class Employee(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    position: str
+    salary: float
+    married: bool
+    start_work: date
+
+
+employees = []
 
 
 def create_app():
@@ -7,5 +32,14 @@ def create_app():
     @app.get("/")
     def index():
         return {"message": "Hello, World!"}
+
+    @app.get("/employee/", response_model=List[EmployeeIn])
+    def read_employees():
+        return employees
+
+    @app.post("/employee/", response_model=Employee)
+    def add_employee(employee: EmployeeIn):
+        employees.append(employee)
+        return {**employee.dict(), "id": 1}
 
     return app
