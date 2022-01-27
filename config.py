0a3495cab01a2fca63b1439b4d7b73e9d96d06fe
Nataliia Dyshko <nataliia.dyshko@ukr.net>
2022-01-27 16:09:27 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-import datetime
 from starlette.config import Config
 
 
@@ -11,14 +10,5 @@ class Config(object):
     DEBUG = config("DEBUG", default=True)
     DEVELOPMENT = config("DEVELOPMENT", default=True)
     TESTING = config("TESTING", default=True)
-    CSRF_ENABLED = True  # ?
-    SECRET_KEY = config("SECRET_KEY", default="#saf@$324#%^TEr")
-
-    PERMANENT = True  # session lives after browser restart
-    PERMANENT_SESSION_LIFETIME = datetime.timedelta(days=2)
-
-    JWT_SECRET_KEY = config("JWT_SECRET_KEY", default="$#3254%$^sgfefwE")
-    JWT_BLACKLIST_ENABLED = True
-    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + sqlite_uri
